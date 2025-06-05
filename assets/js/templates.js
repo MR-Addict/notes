394cda82db992d6c8ed26a015167402ad03bf154
Cael <MR-Addict@qq.com>
2025-06-05 10:11:48 +0800

    feat: add lightbox for image preview

---
+++ assets/js/templates.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  lightbox.option({
+    fadeDuration: 500,
+    resizeDuration: 200,
+    albumLabel: "图片 %1/%2",
+
+    wrapAround: true,
+    disableScrolling: true,
+    alwaysShowNavOnTouchDevices: true,
+  });
+});
